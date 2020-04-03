4f4b5a4c143bfd56010f154d252886983df195b9
Lauren Moor <laurenkaede@gmail.com>
2020-04-03 15:49:44 +0100

    Updates

---
+++ precache-manifest.dcb00202cf82cd01a83f491b5a698235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de2f87d66ec0e43be5aa1074bae4975",
+    "url": "/React-Counter/index.html"
+  },
+  {
+    "revision": "3227fb9c575975eb4803",
+    "url": "/React-Counter/static/css/main.18227765.chunk.css"
+  },
+  {
+    "revision": "9e104d6deaadec1fe963",
+    "url": "/React-Counter/static/js/2.07ace82d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Counter/static/js/2.07ace82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3227fb9c575975eb4803",
+    "url": "/React-Counter/static/js/main.c29e3e8c.chunk.js"
+  },
+  {
+    "revision": "b4da47702fe3a172e590",
+    "url": "/React-Counter/static/js/runtime-main.776e5960.js"
+  }
+]);
